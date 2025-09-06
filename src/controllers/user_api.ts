373bd8d20779e421d76ede7f9c418cf2dd7ef7c3
PhattadonBS <phattadon232@gmail.com>
2025-09-06 22:30:16 +0700

    login register api

--- src/controllers/user_api.ts
+++ src/controllers/user_api.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { dbcon } from "../database/pool";
+import bcrypt from 'bcrypt';
+import axios from "axios";
+import { log } from "console";
 
 export const getAllUsers = async (req: Request, res: Response) =>{
     const [rows] = await dbcon.query("SELECT * FROM Users");
@@ -7,9 +10,55 @@ export const getAllUsers = async (req: Request, res: Response) =>{
 }
 
 export const getUserByEmail = async (req: Request, res: Response) =>{
-    const [rows]:any = await dbcon.query("SELECT * FROM Users WHERE email = ?", [req.params.email]);
-    if (!rows.length) return res.status(404).json({ message: "User not found" });
-    res.json(rows[0]);
+    try{
+        const [rows]:any = await dbcon.query("SELECT * FROM Users WHERE email = ?", [req.params.email]);
+         if (!rows.length) return res.status(404).json({ message: "User not found" });
+        res
+        .status(200)
+        .json(rows[0]);
+    }catch(err){
+        res.status(204).json({message : "User not found"}) 
+    }
+}
+
+
+export const register = async (req: Request, res: Response) => {
+  const { email, password, money, fullname } = req.body;
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+    const uData = { email, password: passwordHash, fullname, money };
+
+    const [results]: any = await dbcon.query("INSERT INTO Users SET ?", uData);
+
+    if (results.affectedRows > 0) {
+      return res.status(201).json({ message: "Account Created" });
+    }
+    res.status(400).json({ message: "Failed to create account" });
+  } catch (err) {
+    res.status(500).json({ message: "Dueplicate email" });
+  }
+};
+
+
+
+export const login = async (req: Request, res: Response) => {
+    const {email, password} = req.body
+    try{
+        const [results]:any = await dbcon.query("SELECT * FROM Users WHERE email = ?", email);
+        const userData = results[0];
+        const isMatch = await bcrypt.compare(password, userData.password);
+        if(!isMatch){
+            res
+            .status(401)
+            .json({message: "email or password worng!!!"})
+            return false;
+        }
+        return res.status(200).json({message: "Login Success"});
+    }catch(error){
+        res
+        .status(401)
+        .json({message: "email or password worng!!!"})
+    }
 }
 
 
--- src/routes/router_api.ts
+++ src/routes/router_api.ts
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { getAllUsers, getUserByEmail } from "../controllers/user_api";
+import { getAllUsers, getUserByEmail, login, register } from "../controllers/user_api";
 import { getAllLottos } from "../controllers/lotto_api";
 
 const router = express.Router();
@@ -11,6 +11,8 @@ router.get("/", (req, res) => {
 // user api
 router.get("/users", getAllUsers);
 router.get("/users/:email", getUserByEmail);
+router.post("/users/login", login);
+router.post('/users/register', register);
 
 // lotto api
 router.get("/lottos", getAllLottos);
